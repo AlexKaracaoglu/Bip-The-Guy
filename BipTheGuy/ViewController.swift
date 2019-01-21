85c0420d0818557b40b9c2c0a9ea5727f69db9e9
Alex Karacaoglu <alexkaracaoglu@alexs-mbp.home>
2019-01-21 11:41:42 -0500

    Section 4.2 - Animations!

--- BipTheGuy/ViewController.swift
+++ BipTheGuy/ViewController.swift
@@ -11,13 +11,37 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    // PROPERTIES
     @IBOutlet weak var imageToBip: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    // FUNCTIONS
+    func animate() {
+        let originalBounds = self.imageToBip.bounds
+        
+        let shrinkage = CGFloat(60)
+        
+        self.imageToBip.bounds = CGRect(x: self.imageToBip.bounds.origin.x + shrinkage,
+                                        y: self.imageToBip.bounds.origin.y + shrinkage,
+                                        width: self.imageToBip.bounds.width - shrinkage,
+                                        height: self.imageToBip.bounds.height - shrinkage)
+        UIView.animate(withDuration: 0.25,
+                       delay: 0.0,
+                       usingSpringWithDamping: 0.2,
+                       initialSpringVelocity: 10,
+                       options: [],
+                       animations: {
+                        self.imageToBip.bounds = originalBounds },
+                       completion: nil
+        )
+    }
+    
+    
+    // ACTIONS
     @IBAction func libraryPressed(_ sender: UIButton) {
     }
     
@@ -26,6 +50,7 @@ class ViewController: UIViewController {
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
         print("BIP BIP!")
+        animate()
     }
 }
 
